480ead5575b22f19306ff11655a92b5392e02090
Festus <savalifestus82@gmail.com>
2025-05-26 00:37:20 +0300

    Updated finance_calculator.py script

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -5,4 +5,4 @@ monthly_savings = monthly_income - total_monthly_expenses
 projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
 
 print(f"Monthly Savings: ${monthly_savings}")
-print(f"Projected Annual Savings: ${projected_savings}")
+print(f"Projected Annual Savings: ${int(projected_savings)}.")
