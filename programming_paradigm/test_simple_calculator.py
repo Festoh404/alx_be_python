6890fd6c3cff9e8b5b4638010114ef4aa17d59dd
Festus <savalifestus82@gmail.com>
2025-07-09 14:39:29 +0300

    Modify test case function names to match expected naming conventions

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -13,17 +13,17 @@ class TestSimpleCalculator(unittest.TestCase):
     def test_addition(self):
         self.assertEqual(self.calc.add(-20,-5), -25)
 
-    def test_subtract(self):
+    def test_subtraction(self):
         self.assertEqual(self.calc.subtract(105, 10), 95)
 
-    def test_multiply(self):
+    def test_multiplication(self):
         self.assertEqual(self.calc.multiply(90, 3), 270)
-    def test_multiply(self):
+    def test_multiplication(self):
         self.assertEqual(self.calc.multiply(-3, -7), 21)
 
-    def test_divide(self):
+    def test_division(self):
         self.assertEqual(self.calc.divide(32, 8), 4)
-    def test_divide(self):
+    def test_division(self):
         self.assertEqual(self.calc.divide(35, 0), None)
 
 if __name__ == "__main__":
