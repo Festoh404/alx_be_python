1873eceb925cdb7beb90d63f489eefa3cd35c632
Festus <savalifestus82@gmail.com>
2025-07-09 14:28:03 +0300

    Initial commit: Add unit tests for SimpleCalculator class using unittest module

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,27 @@
+import unittest
+
+from simple_calculator import SimpleCalculator
+
+
+
+class TestSimpleCalculator(unittest.TestCase):
+    def setUp(self):
+        self.calc = SimpleCalculator()
+
+    def test_add_positive_numbers(self):
+        self.assertEqual(self.calc.add(10,30), 40)
+    def test_add_negative_numbers(self):
+        self.assertEqual(self.calc.add(-20,-5), -25)
+
+    def test_subtract(self):
+        self.assertEqual(self.calc.subtract(105, 10), 95)
+
+    def test_multiply_positive_numbers(self):
+        self.assertEqual(self.calc.multiply(90, 3), 270)
+    def test_multiply_negative_numbers(self):
+        self.assertEqual(self.calc.multiply(-3, -7), 21)
+
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(32, 8), 4)
+    def test_divide_by_zero(self):
+        self.assertEqual(self.calc.divide(35, 0), None)
